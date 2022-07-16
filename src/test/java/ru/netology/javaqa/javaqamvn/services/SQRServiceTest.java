04a4c3399e0da67860ba8068ab7907524f864c70
Sergeevna <l.shliak@docsinbox.ru>
2022-07-16 11:45:25 +0300

    Pom.xml fixed

--- src/test/java/ru/netology/javaqa/javaqamvn/services/SQRServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/SQRServiceTest.java
@@ -1,10 +1,9 @@
 package ru.netology.javaqa.javaqamvn.services;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SQRServiceTest {
 
